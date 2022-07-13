4f2e0207772e5bc89818546e0dee0d167291087d
anacarol.costa <carol.costa2502@gmail.com>
2022-07-12 21:55:24 -0300

    alterar readme.md

--- mercado-livro/src/main/kotlin/com/anacarolcosta/mercadolivro/MercadoLivroApplication.kt
+++ mercado-livro/src/main/kotlin/com/anacarolcosta/mercadolivro/MercadoLivroApplication.kt
@@ -3,9 +3,9 @@ package com.anacarolcosta.mercadolivro
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication //indica onde inicia a aplicação
 class MercadoLivroApplication
 
 fun main(args: Array<String>) {
 	runApplication<MercadoLivroApplication>(*args)
-}
+} //função primaria de todo projeto
