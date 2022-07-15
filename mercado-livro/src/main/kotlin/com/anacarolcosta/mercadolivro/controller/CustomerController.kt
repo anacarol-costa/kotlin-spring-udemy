a86cc910ba8d20dfb052238889830a8c93462180
anacarol.costa <carol.costa2502@gmail.com>
2022-07-15 10:51:54 -0300

    informar path/caminho do endpoint

--- mercado-livro/src/main/kotlin/com/anacarolcosta/mercadolivro/controller/CustomerController.kt
+++ mercado-livro/src/main/kotlin/com/anacarolcosta/mercadolivro/controller/CustomerController.kt
@@ -1,13 +1,15 @@
 package com.anacarolcosta.mercadolivro.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("customer") //pasta ou caminho do endpoint
 class CustomerController {
 
     @GetMapping //recebe dados
     fun helloWorld(): String {
-        return "Hello World"
+        return "Customer 1"
     }
 }
