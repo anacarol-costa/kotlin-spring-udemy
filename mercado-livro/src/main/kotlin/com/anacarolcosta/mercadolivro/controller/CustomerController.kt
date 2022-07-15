4730b34d0e21b4382157fd833020457e5f2109a2
anacarol.costa <carol.costa2502@gmail.com>
2022-07-15 11:55:07 -0300

    criar camada model e retornar customer no GET

--- mercado-livro/src/main/kotlin/com/anacarolcosta/mercadolivro/controller/CustomerController.kt
+++ mercado-livro/src/main/kotlin/com/anacarolcosta/mercadolivro/controller/CustomerController.kt
@@ -1,15 +1,16 @@
 package com.anacarolcosta.mercadolivro.controller
 
+import com.anacarolcosta.mercadolivro.model.CustomerModel
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("customer") //pasta ou caminho do endpoint
+@RequestMapping("customers") //pasta ou caminho do endpoint
 class CustomerController {
 
     @GetMapping //recebe dados
-    fun helloWorld(): String {
-        return "Customer 1"
-    }
+    fun getCustomer(): CustomerModel {
+        return CustomerModel("1", "Carol", "carol@email.com")
+    } //onde irá retornar o customerModel
 }
---
+++ mercado-livro/src/main/kotlin/com/anacarolcosta/mercadolivro/model/CustomerModel.kt
@@ -0,0 +1,7 @@
+package com.anacarolcosta.mercadolivro.model
+
+data class CustomerModel (
+    var id: String,
+    var name: String,
+    var email: String
+)//entidade customer do projeto com seus respectivos atributos
