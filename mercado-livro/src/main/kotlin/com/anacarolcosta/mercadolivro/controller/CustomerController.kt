08f3a7f30465230850a11cb68e97d6eafb5ad4a0
anacarol.costa <carol.costa2502@gmail.com>
2022-07-15 15:03:26 -0300

    alterar status de retorno do POST. Http status CREATED

--- mercado-livro/src/main/kotlin/com/anacarolcosta/mercadolivro/controller/CustomerController.kt
+++ mercado-livro/src/main/kotlin/com/anacarolcosta/mercadolivro/controller/CustomerController.kt
@@ -2,10 +2,12 @@ package com.anacarolcosta.mercadolivro.controller
 
 import com.anacarolcosta.mercadolivro.controller.request.PostCustomerRequest
 import com.anacarolcosta.mercadolivro.model.CustomerModel
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -18,6 +20,7 @@ class CustomerController {
     } //onde irá retornar o customerModel
 
     @PostMapping //criar recurso/dado
+    @ResponseStatus(HttpStatus.CREATED) //notacao q indica a criacao de um objeto
     fun create(@RequestBody customer: PostCustomerRequest) {
         println(customer)
     }
