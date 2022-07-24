3473e8526cec90d475ce237c592783d983b91830
anacarol.costa <carol.costa2502@gmail.com>
2022-07-24 20:42:57 -0300

    adicionar interface pageable para informar paginaçao de livros e status ativo

--- mercado-livro/src/main/kotlin/com/anacarolcosta/mercadolivro/repository/BookRepository.kt
+++ mercado-livro/src/main/kotlin/com/anacarolcosta/mercadolivro/repository/BookRepository.kt
@@ -3,11 +3,13 @@ package com.anacarolcosta.mercadolivro.repository
 import com.anacarolcosta.mercadolivro.enums.BookStatus
 import com.anacarolcosta.mercadolivro.model.BookModel
 import com.anacarolcosta.mercadolivro.model.CustomerModel
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
 
-interface BookRepository: CrudRepository<BookModel, Int> {
-    fun findByStatus(status: BookStatus): List<BookModel>
+interface BookRepository: JpaRepository<BookModel, Int> {
+    fun findByStatus(status: BookStatus, pageable: Pageable): Page<BookModel>
 
     fun findByCustomer(customer: CustomerModel): List<BookModel>
-
 }
