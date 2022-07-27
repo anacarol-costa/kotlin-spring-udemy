43f19cbc0c7f534f65c2568a7a82276362894f82
anacarol.costa <carol.costa2502@gmail.com>
2022-07-27 10:38:49 -0300

    adicionar coluna password na tabela customer

--- mercado-livro/src/main/kotlin/com/anacarolcosta/mercadolivro/controller/request/PostCustomerRequest.kt
+++ mercado-livro/src/main/kotlin/com/anacarolcosta/mercadolivro/controller/request/PostCustomerRequest.kt
@@ -11,5 +11,8 @@ data class PostCustomerRequest (
 
     @field:Email(message = "E-mail deve ser válido") //apenas email valido
     @EmailAvailable
-    var email: String
+    var email: String,
+
+    @field:NotEmpty(message = "Senha deve ser informada!")
+    var password: String
 )
