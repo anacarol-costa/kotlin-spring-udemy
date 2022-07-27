15a57297afbc7d07e6bd8863cc724c9c4d3453b4
anacarol.costa <carol.costa2502@gmail.com>
2022-07-27 12:20:07 -0300

    iniciar configuração de segurança. Liberar algumas rotas

--- mercado-livro/src/main/kotlin/com/anacarolcosta/mercadolivro/config/SecurityConfig.kt
+++ mercado-livro/src/main/kotlin/com/anacarolcosta/mercadolivro/config/SecurityConfig.kt
@@ -2,10 +2,32 @@ package com.anacarolcosta.mercadolivro.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 @Configuration
-class SecurityConfig {
+@EnableWebSecurity
+class SecurityConfig() : WebSecurityConfigurerAdapter() {
+
+    private val PUBLIC_MATCHERS = arrayOf<String>()//url aberta
+
+    private val PUBLIC_POST_MATCHERS = arrayOf(
+        "/customer"
+    ) //rotas publicas
+
+    override fun configure(http: HttpSecurity) {
+        http.cors().and().csrf().disable()
+        http.authorizeHttpRequests()
+            .antMatchers(*PUBLIC_MATCHERS).permitAll()
+            .antMatchers(HttpMethod.POST, *PUBLIC_POST_MATCHERS).permitAll()
+            .anyRequest().authenticated()//requests tem q está autenticadas
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)//requisições independentes
+    }
 
     @Bean
     fun bCryptPasswordEncoder(): BCryptPasswordEncoder {
