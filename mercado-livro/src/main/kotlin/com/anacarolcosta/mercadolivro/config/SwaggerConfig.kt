1545b8524bd06b6cf7e2a511865bfeb69af23d87
anacarol.costa <carol.costa2502@gmail.com>
2022-08-03 18:34:06 -0300

    configurar profiles do spring e swagger para rodar somente em ambiente diferente de prod

--- mercado-livro/src/main/kotlin/com/anacarolcosta/mercadolivro/config/SwaggerConfig.kt
+++ mercado-livro/src/main/kotlin/com/anacarolcosta/mercadolivro/config/SwaggerConfig.kt
@@ -2,6 +2,7 @@ package com.anacarolcosta.mercadolivro.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
@@ -11,6 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 //onde será criado o bean
 
+@Profile("!prod") //só irá rodar se o ambiente for diferente de prod
 @Configuration
 @EnableSwagger2
 class SwaggerConfig {
